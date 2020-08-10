97a1bec8b245a69bf5a345cd89100fc85d0aac22
Kate Dubitski <kate.dubitski@gmail.com>
2020-08-10 14:48:52 -0400

    Delete generateButtons method

--- src/Hangman.js
+++ src/Hangman.js
@@ -45,20 +45,6 @@ class Hangman extends Component {
     }));
   }
 
-  /** generateButtons: return array of letter buttons to render */
-  generateButtons() {
-    return "abcdefghijklmnopqrstuvwxyz".split("").map(ltr => (
-      <button
-        value={ltr}
-        key={ltr}
-        onClick={this.handleGuess}
-        disabled={this.state.guessed.has(ltr)}
-      >
-        {ltr}
-      </button>
-    ));
-  }
-
   restart(){
     this.setState({nWrong: 0, guessed: new Set(), answer: randomWord() })
   }
